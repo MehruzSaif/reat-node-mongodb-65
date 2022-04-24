d4c58078fec34701922116ae941c1172f6909609
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-04-24 22:41:25 +0600

    added put for update and add data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
@@ -8,6 +9,29 @@ const Home = () => {
         .then(res => res.json())
         .then(data => setUsers(data))
     }, [])
+
+    const handleUserDelete = id => {
+        const proceed = window.confirm("Are you sure, you want to delete?");
+
+        if(proceed){
+            console.log('deleting user with id,', id);
+            const url = `http://localhost:5000/user/${id}`;
+
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+
+                if(data.deletedCount > 0) {
+                    console.log('deleted');
+                    const remaining = users.filter(user => user._id !== id);
+                    setUsers(remaining);
+                }
+            })
+        }
+    }
+
     return (
         <div>
             <h2>Available Users: {users.length}</h2>
@@ -15,7 +39,9 @@ const Home = () => {
                 {
                     users.map(user => <li
                     key={user._id}
-                    >{user.name}:: {user.email} <button>x</button>
+                    >{user.name} : : {user.email}
+                    <Link to={`/update/${user._id}`}><button>Update</button></Link>
+                    <button onClick={() =>  handleUserDelete(user._id)}>x</button>
                     </li>)
                 }
             </ul>
@@ -24,3 +50,4 @@ const Home = () => {
 };
 
 export default Home;
+
