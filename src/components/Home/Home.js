c91905fce4876da7152b3dbc2337d223b259f067
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-04-24 06:46:38 +0600

    add data to UI as well as mongodb

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+        .then(res => res.json())
+        .then(data => setUsers(data))
+    }, [])
     return (
         <div>
-            
+            <h2>Available Users: {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li
+                    key={user._id}
+                    >{user.name}:: {user.email} <button>x</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
